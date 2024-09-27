b1d2af5c8ef2dc68e0f0c31539a4b5e0a4827b2c
Dorcky <161785219+Dorcky@users.noreply.github.com>
2024-09-26 23:06:59 -0400

    mise en place pied de page

--- JS/header.js
+++ JS/header.js
@@ -1,6 +1,6 @@
 // JS/loadHeader.js
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('assets/nav.html')
+    fetch('assets/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
